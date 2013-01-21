27299e6036f18219318748aabcae2b4f7d99dbae
sd5 <timo.braendlin@googlemail.com>
2013-01-21 22:44:43 +0100

    Initial commit.

---
+++ src/me/sd5/ancientterrain/ATMain.java
@@ -0,0 +1,17 @@
+package me.sd5.ancientterrain;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ATMain extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+	
+}
