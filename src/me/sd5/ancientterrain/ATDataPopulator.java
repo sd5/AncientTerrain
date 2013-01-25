e3da889e22a73620e491b849bd8c8257fbd0dc55
sd5 <timo.braendlin@googlemail.com>
2013-01-25 16:00:18 +0100

    Added empty populator which will add the data values on the generated blocks.

---
+++ src/me/sd5/ancientterrain/ATDataPopulator.java
@@ -0,0 +1,18 @@
+package me.sd5.ancientterrain;
+
+import java.util.Random;
+
+import org.bukkit.Chunk;
+import org.bukkit.World;
+import org.bukkit.generator.BlockPopulator;
+
+public class ATDataPopulator extends BlockPopulator {
+
+	@Override
+	public void populate(World world, Random random, Chunk chunk) {
+
+		
+		
+	}
+
+}
